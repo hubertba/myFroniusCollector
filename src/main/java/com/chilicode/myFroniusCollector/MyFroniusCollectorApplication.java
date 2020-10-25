e8a59ab2c2046efac936159743cd46f9f8667de9
Sarah Baumgartner <sarahbaumgartner@Sarahs-MacBook-Pro.local>
2020-10-25 13:43:13 +0100

    initi with project from https://start.spring.io

---
+++ src/main/java/com/chilicode/myFroniusCollector/MyFroniusCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.chilicode.myFroniusCollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFroniusCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFroniusCollectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chilicode/myFroniusCollector/MyFroniusCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chilicode.myFroniusCollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFroniusCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
